4cf8fdb89633af50ba5a5ddc20300e8436213443
benikib <benikasu7@gmail.com>
2025-07-26 21:13:17 +0200

    deployement de webhooks 2

--- resources/views/layouts/etablissements/aside.blade.php
+++ resources/views/layouts/etablissements/aside.blade.php
@@ -68,6 +68,16 @@
                     <span class="nav-link-text ms-1">Utilisateurs</span>
                 </a>
             </li>
+            <li class="nav-item">
+                <a class="nav-link {{ request()->routeIs('personnels.*') ? 'active' : '' }}"
+                    href="{{ route('personnels.index') }}">
+                    <div
+                        class="icon icon-shape icon-sm shadow border-radius-md bg-gradient-danger text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="fas fa-users-cog text-white"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Personnels</span>
+                </a>
+            </li>
             {{-- <li class="nav-item">
                 <a class="nav-link" href="{{ route('users.promotions') }}"
                     class="{{ request()->routeIs('users.promotions.*') ? 'active' : '' }}">
