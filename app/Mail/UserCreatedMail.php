db77d1f4a625e6d77c5e6d9b8857a1d6501b0c50
BENI KASU <benikasu7@gmail.com>
2026-07-12 12:27:29 +0200

    fix: correction message mail create name view

--- app/Mail/UserCreatedMail.php
+++ app/Mail/UserCreatedMail.php
@@ -22,10 +22,10 @@ class UserCreatedMail extends Mailable
     public function build()
     {
         return $this->subject('Bienvenue ! Créez votre mot de passe')
-            ->view('emails.user_created')
+            ->view('mail.user_created')
             ->with([
                 'user' => $this->user,
                 'resetUrl' => $this->resetUrl,
-            ]  );
+            ]);
     }
 }
